a589638939e0811c810f8ef7e19c5dfb529623bb
PP3Ti <porkolab.peti@gmail.com>
2023-05-09 19:18:03 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            {
+              loader: 'style-loader',
+            }, {
+              loader: 'css-loader',
+            }, {
+              loader: 'resolve-url-loader',
+            }, {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true, // <-- !!IMPORTANT!!
+              }
+            }
+          ]
+        },
+    ]
+  },
+};
